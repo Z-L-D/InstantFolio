bd0ee79cee0492b1c3ff2aefc85ecace8d5604ca
Z-L-D <zacldoyle@gmail.com>
2023-05-14 14:58:19 -0500

    -

---
+++ js/posts.js
@@ -0,0 +1,146 @@
+//TEMPALTE rev 2
+// var post_## = {post_type:"visuals, audio, science, blog, downloads",
+//                post_subtype:"visuals(archviz, graphics, photography, colorizations, motion), audio(electronic, theatrical, samples, sound effects), science(interactives, furniture, software, hardware, research)",
+//                post_title:"",
+//                post_description:"",
+//                post_date:"",
+//                post_image:"",
+//------------------------------------------------------------------------
+//                modal_title:"",
+//                modal_summary:"",
+//------------------------------------------------------------------------
+//                file_struct:"file_name",
+//                image_preview:"",
+//                image_preview_download:"",
+//                paragraph:"This is some demo text to test the JSON parsing functionality when used in the setting of a blog. If this works, it will make developing a blog entry tool very easy.",
+//                specifications:"",
+//                specifications_elements:"",
+//                specifications_process:"",
+//                specifications_software:"",
+//                specifications_duration:"",
+//                interactives:"",
+//                image_gallery:"",
+//                image_gallery_download:"",
+//                image_compare_slide:"",
+//                image_compare_fade:"",
+//                print_request:"",
+//                music_request:"",
+//                exhibit_request:"",
+//                furniture_request:"",
+//                software_request:"",
+//                software_download:"",
+//                hardware_request:"",
+//                share:"y",
+//                license:"CC CC-C MIT CR "
+//               }              
+//post_array.push(post_##);
+
+var post_array = [];
+
+var post_x = {post_type:"",
+               post_subtype:"",
+               post_title:"Empty",
+               post_description:"",
+               post_date:"",
+               post_image:""
+//------------------------------------------------------------------------
+//                modal_title:"",
+//                modal_summary:"",
+//------------------------------------------------------------------------
+//                file_struct:"file_name",
+//                image_preview:"",
+//                image_preview_download:"",
+//                paragraph:"This is some demo text to test the JSON parsing functionality when used in the setting of a blog. If this works, it will make developing a blog entry tool very easy.",
+//                specifications:"",
+//                specifications_elements:"",
+//                specifications_process:"",
+//                specifications_software:"",
+//                specifications_duration:"",
+//                interactives:"",
+//                image_gallery:"",
+//                image_gallery_download:"",
+//                image_compare_slide:"",
+//                image_compare_fade:"",
+//                print_request:"",
+//                music_request:"",
+//                exhibit_request:"",
+//                furniture_request:"",
+//                software_request:"",
+//                software_download:"",
+//                hardware_request:"",
+//                share:"y",
+//                license:"CC CC-C MIT CR "
+              }              
+// post_array.push(post_x);
+
+var post_0 = {post_type:"secb",
+               post_subtype:"secl",
+               post_title:"Electronic Music Playlist",
+               post_description:"",
+               post_date:"",
+               post_image:""
+//------------------------------------------------------------------------
+//                modal_title:"",
+//                modal_summary:"",
+//------------------------------------------------------------------------
+//                file_struct:"file_name",
+//                image_preview:"",
+//                image_preview_download:"",
+//                paragraph:"This is some demo text to test the JSON parsing functionality when used in the setting of a blog. If this works, it will make developing a blog entry tool very easy.",
+//                specifications:"",
+//                specifications_elements:"",
+//                specifications_process:"",
+//                specifications_software:"",
+//                specifications_duration:"",
+//                interactives:"",
+//                image_gallery:"",
+//                image_gallery_download:"",
+//                image_compare_slide:"",
+//                image_compare_fade:"",
+//                print_request:"",
+//                music_request:"",
+//                exhibit_request:"",
+//                furniture_request:"",
+//                software_request:"",
+//                software_download:"",
+//                hardware_request:"",
+//                share:"y",
+//                license:"CC CC-C MIT CR "
+              }              
+post_array.push(post_0);
+
+var post_1 = {post_type:"blog",
+               post_subtype:"",
+               post_title:"Blog 1",
+               post_description:"",
+               post_date:"",
+               post_image:""
+//------------------------------------------------------------------------
+//                modal_title:"",
+//                modal_summary:"",
+//------------------------------------------------------------------------
+//                file_struct:"file_name",
+//                image_preview:"",
+//                image_preview_download:"",
+//                paragraph:"This is some demo text to test the JSON parsing functionality when used in the setting of a blog. If this works, it will make developing a blog entry tool very easy.",
+//                specifications:"",
+//                specifications_elements:"",
+//                specifications_process:"",
+//                specifications_software:"",
+//                specifications_duration:"",
+//                interactives:"",
+//                image_gallery:"",
+//                image_gallery_download:"",
+//                image_compare_slide:"",
+//                image_compare_fade:"",
+//                print_request:"",
+//                music_request:"",
+//                exhibit_request:"",
+//                furniture_request:"",
+//                software_request:"",
+//                software_download:"",
+//                hardware_request:"",
+//                share:"y",
+//                license:"CC CC-C MIT CR "
+              }              
+post_array.push(post_1);
