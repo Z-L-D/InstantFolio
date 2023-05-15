cd889ffba15a0cde8a22dbc8b2b47c44b805cc28
Z-L-D <zacldoyle@gmail.com>
2023-05-15 16:05:50 -0500

    -

--- js/main_work.js
+++ js/main_work.js
@@ -28,6 +28,7 @@ function tag_init()
 }
 tag_init();
 
+
 // function tags_set_group_state(group_name, state)
 // {
 //     var group = document.getElementsByClassName(group_name);
@@ -47,3 +48,25 @@ tag_init();
 //         }
 //     }
 // }
+
+function load_work()
+{
+    for (i = 0; i < post_array.length; i++){
+        document.getElementById("posts").insertAdjacentHTML('beforeend', `
+                <div class="item ${post_array[i].post_type} ${post_array[i].post_subtype}" onclick="modal_load(post_${i})">
+                    <div class="card">
+                        <div class="card_background" style="background-image: url('img/${post_array[i].post_type}/${post_array[i].post_title}/${post_array[i].post_title}--th.jpg')"></div>
+                        <div class="text">
+                            <div class="card_title">${post_array[i].post_title}</div>
+                            <div class="description"><p class="text">${post_array[i].post_description}<br>${post_array[i].post_type}</p></div>
+                        </div>
+                    </div>
+                </div>
+        `);
+    }
+
+    document.getElementById("tags").insertAdjacentHTML('beforeend', `
+        <item id="tags_all" class="item ${tag_animation}" onclick="tags_set_group('main', 'all')"><a>ALL</a></item>
+    `);
+};
+load_work();
