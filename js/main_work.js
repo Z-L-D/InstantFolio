69e7bf7f37a411cc24ff228d9bd9e6b55f2e3b22
Z-L-D <zacldoyle@gmail.com>
2023-05-17 02:33:10 -0500

    -

--- js/main_work.js
+++ js/main_work.js
@@ -49,94 +49,43 @@ tag_init();
 //     }
 // }
 
-var post_types = ['all', ['tag_temp', 'subtag_tem']]
+// var post_types = ['all', ['tag_temp', 'subtag_temp']]
 
 function load_work()
 {
-    // for(let i = 0; i < post_array.length; i++){
-    //     document.getElementById("posts").insertAdjacentHTML('beforeend', `
-    //             <div class="item ${post_array[i].post_type} ${post_array[i].post_subtype}" onclick="modal_load(post_${i})">
-    //                 <div class="card">
-    //                     <div class="card_background" style="background-image: url('img/${post_array[i].post_type}/${post_array[i].post_title}/${post_array[i].post_title}--th.jpg')"></div>
-    //                     <div class="text">
-    //                         <div class="card_title">${post_array[i].post_title}</div>
-    //                         <div class="description"><p class="text">${post_array[i].post_description}<br>${post_array[i].post_type}</p></div>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //     `);
-
-    //     if(post_types.includes(post_array[i].post_type) === false){
-    //         if(post_array[i].post_subtype != ""){
-    //             if(post_types.includes(post_array[i].post_type) === false){
-    //                 post_types.push(`[${post_array[i].post_type}]`);
-    //             }
-    //         }
-    //         else if(post_array[i].post_subtype === ""){
-    //             post_types.push(post_array[i].post_type);
-    //         }
-            
-    //     }
-    // }
-
-    // for(let i = 0; i < post_types.length; i++){
-    //     document.getElementById("tags").insertAdjacentHTML('beforeend', `
-    //         <item class="item ${tag_animation}"><a>${post_types[i].toUpperCase()}</a></item>
-    //     `);
-    // }
-    
-    // post_types = ['tag1', ['tag2', 'tag2_subtag1'], 'tag3']
-
-    // console.log(post_types.includes('tag2'));
-
     for(let i = 0; i < post_array.length; i++){
-        // console.log(`current evaluating tag: ${post_array[i].post_type}`)
-        if(post_array[i].post_subtype != ""){
-            for(let j = 0; j < post_types.length; j++){
-                if(Array.isArray(post_types[j]) === true){
-                    console.log(`is array at index ${i}`)
-                    if(post_types[j].includes(post_array[i].post_type) === false){
-                        // post_types.push([post_array[i].post_type, post_array[i].post_subtype])
-                        console.log(`tag not found within index ${i}`)
-                    }
-                    else{
-                        console.log(`tag found within index ${i}`)
-                    }
-                }
-                else{
-                    // post_types.push([post_array[i].post_type, post_array[i].post_subtype])
-                }
-            //     else if(Array.isArray(post_types[j]) === true){
-            //         post_types.push([post_array[i].post_type, post_array[i].post_subtype])
-            //     }
-            }
-            // post_types.push([post_array[i].post_type, post_array[i].post_subtype])
-        }
-        else{
-            if(post_types.includes(post_array[i].post_type) === false){
-                post_types.push(post_array[i].post_type);
-            }
-        }
-
-        // if(Array.isArray(post_types[i])){
-        //     if(post_types[i].includes(post_array[i].post_type) === false){
-        //         console.log(`added ${post_array[i].post_type} to subarray`)
-        //     }
-        //     else{
-        //         console.log(`item already exists in subarray`)
-        //     }
-        // }
-        // else{
-        //     if(post_types.includes(post_array[i].post_type) === false){
-        //         console.log(`added ${post_array[i].post_type} to array`)
-        //     }
-        //     else{
-        //         console.log('item already exists in array')
-        //     }
-        // }
+        document.getElementById("posts").insertAdjacentHTML('beforeend', `
+                <div class="item ${post_array[i].post_type} ${post_array[i].post_subtype}" onclick="modal_load(post_${i})">
+                    <div class="card">
+                        <div class="card_background" style="background-image: url('img/${post_array[i].post_type}/${post_array[i].post_title}/${post_array[i].post_title}--th.jpg')"></div>
+                        <div class="text">
+                            <div class="card_title">${post_array[i].post_title}</div>
+                            <div class="description"><p class="text">${post_array[i].post_description}<br>${post_array[i].post_type}</p></div>
+                        </div>
+                    </div>
+                </div>
+        `);
     }
 
-    console.log(post_types);
+    let post_types = ['all']
+
+    for(let i = 0; i < post_array.length; i++){
+        if(post_types.includes(post_array[i].post_type) === false){
+            post_types.push(post_array[i].post_type);
+            console.log(`post_array index ${i} type does not exist current in post_types array, adding to array`)
+        }
+        else{
+            console.log(`post_array index ${i} type already exists in current in post_types array, ignoring`)
+        }
+    }
+
+    console.log(post_types)
+
+    for(let i = 0; i < post_types.length; i++){
+        document.getElementById("tags").insertAdjacentHTML('beforeend', `
+            <item class="item ${tag_animation}"><a>${post_types[i].toUpperCase()}</a></item>
+        `);
+    }
 
 };
 load_work();
