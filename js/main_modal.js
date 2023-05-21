2b2529d5faaff3cba72647ac11abe013b640a1d5
Z-L-D <zacldoyle@gmail.com>
2023-05-21 14:19:58 -0500

    -

--- js/main_modal.js
+++ js/main_modal.js
@@ -1,22 +1,27 @@
 function load_modal(post_number)
 {
+    console.log("post number: " + post_number)
     document.getElementById("post_modal_box").insertAdjacentHTML('beforeend', `
-            MODAL MODAL MODAL MODAL MODAL MODAL
+            ${post_array[post_number].post_title}
             <br>
-            ${post_number.post_title}
-            <br>
-            ${post_number.post_description}
-            <br>
-            ${post_number.post_type}
-            <br>
-            ${post_number.post_subtype}
+            ${post_array[post_number].post_description}
     `);
-    // document.getElementById("post_modal").style.display = "block";
+    if(post_array[post_number].post_subtype.length > 0){
+        document.getElementById("post_modal_box").insertAdjacentHTML('beforeend', `
+            <br>
+            ${post_array[post_number].post_type} > ${post_array[post_number].post_subtype}
+        `);
+    }
+    else{
+        document.getElementById("post_modal_box").insertAdjacentHTML('beforeend', `
+            <br>
+            ${post_array[post_number].post_type}
+        `);
+    };
     document.getElementById("post_modal").classList.add("modal_show");
 }
 
 document.getElementById("post_modal_close").addEventListener("click", function(){
-    // document.getElementById("post_modal").style.display = "none";
     document.getElementById("post_modal_box").innerHTML = "";
     document.getElementById("post_modal").classList.remove("modal_show");
 })
