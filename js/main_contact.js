aadbe9dd0208ef09000ef9fc66ec8df3cddb17a6
Z-L-D <zacldoyle@gmail.com>
2023-05-24 15:54:09 -0500

    -

--- js/main_contact.js
+++ js/main_contact.js
@@ -23,22 +23,8 @@ function load_contact(){
             document.querySelector('.contact_content').style.gridTemplateColumns = "1fr";
             document.querySelector('.contact_content_email').style.display = "none";
         }
-        // else if(site_settings.site_contact_email === "Email Push"){
-        //     document.querySelector('.contact_content_email').innerHTML = `
-        //         <h2>Get In Touch</h2>
-        //     `;
-        // };
-        // else if(site_settings.site_contact_email === "HTML Comment Box"){
-        //     document.querySelector('.contact_content_email').innerHTML = `
-        //         <h2>Get In Touch</h2>
-        //         <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Comment Box</a> is loading comments...</div>
-        //         <link rel="stylesheet" type="text/css" href="https://www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
-        //         <script type="text/javascript" id="hcb"> /**/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="https://www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24LeLW41vfIdk7AbjJo%2FH8X0"+"&opts=16862&num=10&ts=1610077761074");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /**/ </script>
-        //     `
-        // }
-
-
-
+        
+        const email_form = document.getElementById('contact_content_email_form_constructor')
         
 
         const social_media_list = [ {id:"bandcamp",         name:"bandcamp",        userlink:"https://@@REPLACE@@.bandcamp.com",                sharelink:"",       symbol:"M0 18.75l7.437-13.5H24l-7.438 13.5H0z", viewbox: "0 0 24 24"},
