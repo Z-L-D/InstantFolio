c1bb5b921f8de887fa1c86e8107542e7aef50ffa
Z-L-D <zacldoyle@gmail.com>
2023-06-21 12:23:37 -0500

    -

--- js/builder_work.js
+++ js/builder_work.js
@@ -6,4 +6,40 @@ function work_load(){
     work_elements_include.addEventListener('click', function(){
         check_include(work_elements_include, form_section_work);
     });
+
+    var work
+
+    for(i = 0; i < site_definition.work.posts.length; i++){
+        work += work_collation(i);
+    }
+
+    document.getElementById('post_cards').innerHTML = work;
+}
+
+function work_collation(post_number){
+    let post = site_definition.work.posts[post_number]
+    let title = post.title;
+    let type = post.type;
+    let subtype = post.subtype;
+    let description = post.description;
+    let content = post.content;
+    let date = post.date;
+    let image = post.image;
+    let license = post.image;
+    let sharing = post.sharing;
+
+    let html_return = `
+        <div class="post">
+            <div class="title">${title}</div>
+            <div class="type">${type}</div>
+            <div class="subtype">${subtype}</div>
+            <div class="description">${description}</div>
+            <div class="content">${content}</div>
+            <div class="date">${date}</div>
+            <div class="image">${image}</div>
+            <div class="license">${license}</div>
+            <div class="sharing">${sharing}</div>
+        </div>
+    `
+    return html_return;
 }
